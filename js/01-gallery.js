e1be7af47b9e24f26c02c8bae152c93944e60268
MichalBxqt <117540580+MichalBxqt@users.noreply.github.com>
2023-04-17 20:24:43 +0200

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,22 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+for (const img of galleryItems) {
+  const listEl = `<div class=“gallery__item”>
+    <a class=“gallery__link” href=${img.original}>
+      <img
+        class=“gallery__image”
+        src=${img.preview}
+        data-source=${img.original}
+        alt=${img.description}
+      />
+    </a>
+  </div>`;
+  galleryEl.insertAdjacentHTML('beforeend', listEl);
+}
+galleryEl.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.tagName !== 'IMG') return;
+  const bigImg = basicLightbox.create(`<img src=${event.target.dataset.source}>`);
+  bigImg.show();
+});
